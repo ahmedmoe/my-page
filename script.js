5c15cd10d408b0f96e6c7b7fd5c8d1652ff2bcc9
ahmedmoe <ahmedmoe.github.io>
2016-08-24 12:35:43 -0400

    This is the first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('#factions').click(function(){
+    $("list1").slideToggle(350);
+  });
+});
+
+/*
+<script type="text/javascript" src="script.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>
+*/
